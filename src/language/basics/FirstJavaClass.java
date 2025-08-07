d8343a4e1a3ce11625ac554878504aac1578c333
Dhanya Purushothaman <dhanya87.rejil@gmail.com>
2025-08-07 16:11:43 -0400

    Assignment 1

---
+++ src/language/basics/FirstJavaClass.java
@@ -0,0 +1,19 @@
+package language.basics;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i= 2;
+		
+		System.out.println(i);
+		int j= i + 4;
+		System.out.println(j);
+		int k = j/i;
+		System.out.println(k);
+		
+		System.out.println("Thanks");
+
+	}
+
+}
